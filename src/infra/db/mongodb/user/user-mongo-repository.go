605a46b3e245a77857598495afff0a16359ac916
Douglas Dennys <douglasdennys45@gmail.com>
2021-04-09 15:17:26 -0300

    fix: ajuste no insert

--- src/infra/db/mongodb/user/user-mongo-repository.go
+++ src/infra/db/mongodb/user/user-mongo-repository.go
@@ -19,6 +19,6 @@ func NewUserMongoRepository(collection *mongo.Collection) UserMongoRepository {
 }
 
 func (r repository) Add(addUser *user.AddUserParamRepo) error {
-	_, err := r.collection.InsertOne(context.Background(), addUser)
+	_, err := r.collection.InsertOne(context.Background(), addUser.AddUserParam)
 	return err
 }
