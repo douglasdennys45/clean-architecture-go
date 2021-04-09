d75cb6b9585f9b96157c80495ec27f749f3f2e7d
Douglas Dennys <douglasdennys45@gmail.com>
2021-04-09 15:26:51 -0300

    fix: ajuste no model

--- src/infra/db/mongodb/user/user-mongo-repository.go
+++ src/infra/db/mongodb/user/user-mongo-repository.go
@@ -4,10 +4,20 @@ import (
 	"context"
 	"github.com/douglasdennys/go-mongodb/src/app/protocols/db/user"
 	"go.mongodb.org/mongo-driver/mongo"
+	"time"
 )
 
 type UserMongoRepository interface {
 	user.AddUserRepository
+}
+
+type userData struct {
+	Name      string    `json:"name" bson:"name" validate:"required"`
+	Email     string    `json:"email" bson:"email" validate:"required,email"`
+	Password  string    `json:"password" bson:"password" validate:"required"`
+	Active    bool      `json:"active" bson:"active" validate:"required"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at" validate:"datetime"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at" validate:"datetime"`
 }
 
 type repository struct {
@@ -19,6 +29,6 @@ func NewUserMongoRepository(collection *mongo.Collection) UserMongoRepository {
 }
 
 func (r repository) Add(addUser *user.AddUserParamRepo) error {
-	_, err := r.collection.InsertOne(context.Background(), addUser.AddUserParam)
+	_, err := r.collection.InsertOne(context.Background(), &userData{addUser.AddUserParam.Name, addUser.AddUserParam.Email, addUser.AddUserParam.Password, true, time.Now(), time.Now()})
 	return err
 }
